8998adf4c98b2338acb25c1351e705b4e0515a73
Luciano <luciano@desrotulando.com>
2022-08-31 10:30:09 -0300

    init flutter

---
+++ lib/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  // "/": (context) => const PageSplash(),
+  "/home": (context) => const PageHome(),
+};
---
+++ lib/view/screens/page_home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class PageHome extends StatefulWidget {
+  const PageHome({Key? key}) : super(key: key);
+
+  @override
+  State<PageHome> createState() => _PageHomeState();
+}
+
+class _PageHomeState extends State<PageHome> {
+  @override
+  Widget build(BuildContext context) {}
+}
